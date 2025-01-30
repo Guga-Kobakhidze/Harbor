c49db904b301916bbb1bd0b54aaad8be76eda9f9
Guga <guga.kobakhidze@makingscience.com>
2025-01-30 05:25:59 +0400

    Add Form Submition

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 const SELECTED_LANGUAGE = "SelectedLanguage";
+const SERVICE_ID = import.meta.env.VITE_API_HARBOR_ID;
+const TEMPLATE_ID = import.meta.env.VITE_API_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_API_PUBLIC_KEY;
 
-export { SELECTED_LANGUAGE };
+export { SELECTED_LANGUAGE, SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY };
